d40f24dd759b3378c6d5f7be67a09f0ae640f195
Palm <waraporn.v@augmentis.biz>
2016-07-12 16:59:40 +0700

    edit function in class Animal

--- src/com/waraporn/zoo/Animal.java
+++ src/com/waraporn/zoo/Animal.java
@@ -5,4 +5,54 @@ package com.waraporn.zoo;
  */
 public class Animal {
 
+    private  int id;
+    private String name;
+    private String type;
+    private int age;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    protected void addAnimal(String name, String type, int age){
+        this.name = name;
+        this.type = type;
+        this.age = age;
+    }
+
+    protected void deleteAnimal(int id){
+
+    }
+
+    public String listAnimal(){
+        return "";
+    }
 }
